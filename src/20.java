279a27c2fd6481047aed3f28363451b7bd4899d2
ballhausennoradin <ballhausennoradin@gmail.com>
2025-03-30 23:26:43 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,6 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // This is just an example of how you might start your Java program.
+        System.out.println("Hello, World!");
+    }
+}
