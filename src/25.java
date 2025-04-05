babc96c8fe5037def63ef574531e908d9a933646
ballhausennoradin <ballhausennoradin@gmail.com>
2025-04-05 12:55:19 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,13 @@
+public class SchoolProject {
+    private String name;
+    private int studentsCount;
+
+    public SchoolProject(String name, int studentsCount) {
+        this.name = name;
+        this.studentsCount = studentsCount;
+    }
+
+    public void enrollStudent(int studentId) {
+        // Add logic to enroll a student
+    }
+}
