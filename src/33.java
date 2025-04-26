412a934106836251d63afa7754bf99be44bae2c4
ballhausennoradin <ballhausennoradin@gmail.com>
2025-04-26 00:19:54 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,11 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example code to demonstrate basic operations and logical expressions
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+    }
+}
